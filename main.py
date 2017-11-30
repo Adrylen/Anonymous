e8cea2aa41ca5d57dbb9a37b9705c4845b3d1a82
quentin <quentin@quentinpc.home>
2017-11-30 15:06:07 +0100

    projet

--- main.py
+++ main.py
@@ -13,12 +13,18 @@ def main(dirname, path, host, account, passwd, folder, frequency=15, depth=6, de
 	snapshot = Snapshot(dirname)
 	snapshot.scan(int(depth))
 
+	ftp = FTP(host)
+	ftp.login(account,passwd)
+	ftp.cwd(folder)
+
 	while True:
 		s2 = Snapshot(dirname)
 		s2.scan(int(depth))
 
 		# print(len(snapshot.diff(s2)))
 		for diff in snapshot.diff(s2):
+			ftp_command = "STOR " + diff.name
+			ftp.storbinary(ftp_command.encode('utf-8'), open(diff.path))
 			print(str(diff))
 
 		snapshot = s2
