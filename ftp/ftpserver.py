a2a06784d8f905cb97365c20c2fe947c94a4eb9b
bloudman <adriencom@msn.com>
2017-11-30 15:49:42 +0100

    ajout des choses dans test

--- ftp/ftpserver.py
+++ ftp/ftpserver.py
@@ -1,12 +1,29 @@
 from ftplib import FTP
+import ftplib
 
+
+
+
+
+def cdTree(ftp, currentDir):
+	if currentDir != "":
+		try:
+			ftp.cwd(currentDir)
+		except ftplib.error_perm as e:
+			print("Probleme: " + str(e))
+			cdTree(ftp, "/".join(currentDir.split("/")[:-1]))
+			ftp.mkd(currentDir)
+			ftp.cwd(currentDir)
 
 def test():
 	ftp = FTP('localhost')
 	ftp.login('bob', '1234')
-	print(ftp.cwd('Bonjour'))
+	#print(ftp.cwd('Bonjour'))
 	print(ftp.dir())
-	print(ftp.retrbinary('RETR Bonjour.txt', open('Bonjour.txt', 'wb').write))
+	#print(ftp.retrbinary('RETR test/Bonjour.txt', open('Bonjour.txt', 'wb').write))
+	file = open('Bonjour.txt','rb')
+	cdTree(ftp, "/test/test1/test2")
+	ftp.storbinary('STOR /test/test1/test2/Bonjour.txt', file)     # send the file
 	ftp.quit()
 
 
